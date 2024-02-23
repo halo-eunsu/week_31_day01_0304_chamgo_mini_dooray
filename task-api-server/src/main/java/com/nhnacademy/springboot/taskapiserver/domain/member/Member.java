aab4720f4906ea6502035dbc935451ae2412ecaf
halo-eunsu <ujd017@naver.com>
2024-02-23 17:05:20 +0900

    controller commit

--- task-api-server/src/main/java/com/nhnacademy/springboot/taskapiserver/domain/member/Member.java
+++ task-api-server/src/main/java/com/nhnacademy/springboot/taskapiserver/domain/member/Member.java
@@ -3,10 +3,12 @@ package com.nhnacademy.springboot.taskapiserver.domain.member;
 import com.nhnacademy.springboot.taskapiserver.domain.AuthType;
 import com.nhnacademy.springboot.taskapiserver.domain.project.Project;
 import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
 
 @Getter
+@Setter
 @Entity
 public class Member {
 
--- task-api-server/src/main/java/com/nhnacademy/springboot/taskapiserver/domain/member/MemberServiceImpl.java
+++ task-api-server/src/main/java/com/nhnacademy/springboot/taskapiserver/domain/member/MemberServiceImpl.java
@@ -1,16 +1,23 @@
 package com.nhnacademy.springboot.taskapiserver.domain.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.nhnacademy.springboot.taskapiserver.domain.project.Project;
+import com.nhnacademy.springboot.taskapiserver.domain.project.ProjectRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository;
+    private final ProjectRepository projectRepository;
 
-    public MemberServiceImpl(MemberRepository memberRepository) {
+    @Autowired
+    public MemberServiceImpl(MemberRepository memberRepository, ProjectRepository projectRepository) {
         this.memberRepository = memberRepository;
+        this.projectRepository = projectRepository;
     }
 
     @Override
@@ -20,8 +27,11 @@ public class MemberServiceImpl implements MemberService {
 
     @Override
     public Member getMember(Long id) {
-        return memberRepository.findById(id)
-                .orElse(null);
+        Optional<Member> memberOptional = memberRepository.findById(id);
+        if (memberOptional.isPresent()) {
+            return memberOptional.get();
+        }
+        return null;
     }
 
     @Override
@@ -35,5 +45,37 @@ public class MemberServiceImpl implements MemberService {
     @Override
     public void delete(Member member) {
         memberRepository.delete(member);
+    }
+
+    @Override
+    public void addMemberToProject(Long projectId, Long memberId) {
+        Optional<Project> projectOptional = projectRepository.findById(projectId);
+        if (!projectOptional.isPresent()) {
+            throw new IllegalArgumentException("Project not found with ID: " + projectId);
+        }
+        Project project = projectOptional.get();
+
+        Optional<Member> memberOptional = memberRepository.findById(memberId);
+        if (!memberOptional.isPresent()) {
+            throw new IllegalArgumentException("Member not found with ID: " + memberId);
+        }
+        Member member = memberOptional.get();
+
+        member.setProject(project);
+        memberRepository.save(member);
+    }
+
+    @Override
+    public void removeMemberFromProject(Long projectId, Long memberId) {
+        Optional<Member> memberOptional = memberRepository.findById(memberId);
+        if (!memberOptional.isPresent()) {
+            throw new IllegalArgumentException("Member not found with ID: " + memberId);
+        }
+        Member member = memberOptional.get();
+
+        if (member.getProject() != null && member.getProject().getProjectId().equals(projectId)) {
+            member.setProject(null);
+            memberRepository.save(member);
+        }
     }
 }
